b54161f6279e026c18d66154289e3e7fb96d5181
Michal Skala <michal@mskala.com>
2014-10-14 23:02:43 +0200

    ldFilterDropdown updates.

--- app/scripts/directives/ldfilterdropdown.js
+++ app/scripts/directives/ldfilterdropdown.js
@@ -14,16 +14,7 @@ angular.module('ldAdminTools')
 			selectedFilter: '=',
 			filters: '='
 		},
-		template: function () {
-			var tpl = '<div class="ld-filter-dropdown" dropdown>';
-			tpl += '<a style="cursor: pointer" dropdown-toggle role="button">{{ selectedFilter.name }} <i class="fa fa-caret-down"></i></a>';
-			tpl += '<ul class="dropdown-menu">';
-			tpl += '<li ng-repeat="filter in filters"><a ng-click="selectFilter(filter);">{{ filter.name }}</a></li>';
-			tpl += '</ul>';
-			tpl += '</div>';
-
-			return tpl;
-		},
+		templateUrl: 'partials/ldfilterdropdown.html',
 		link: function (scope, element, attrs) {
 
 			scope.selectFilter = function (filter) {
