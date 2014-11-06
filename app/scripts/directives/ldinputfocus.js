df005334cd6c91ea39793a8913c2940337d692e3
Michal Skala <michal@mskala.com>
2014-11-06 17:47:14 +0100

    ld-input-focus directive added

---
+++ app/scripts/directives/ldinputfocus.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name ldAdminTools.directive:ldInputFocus
+ * @description
+ * # ldInputFocus
+ */
+angular.module('ldAdminTools')
+	.directive('ldInputFocus', ['$parse', '$timeout', function ($parse, $timeout) {
+		return {
+			restrict: 'A',
+			link: function postLink(scope, element, attrs) {
+				var property = attrs.ldInputFocus;
+				var isFocus = $parse(property);
+				scope.$watch(isFocus, function (newValue) {
+					console.log(!!newValue);
+					if (!!newValue) {
+						$timeout(function() {
+							element[0].focus();
+						});
+					}
+				});
+			}
+		};
+	}]);
--- dist/ldAdminTools.js
+++ dist/ldAdminTools.js
@@ -189,6 +189,33 @@ angular.module('ldAdminTools')
 
 /**
  * @ngdoc directive
+ * @name ldAdminTools.directive:ldInputFocus
+ * @description
+ * # ldInputFocus
+ */
+angular.module('ldAdminTools')
+	.directive('ldInputFocus', ['$parse', '$timeout', function ($parse, $timeout) {
+		return {
+			restrict: 'A',
+			link: function postLink(scope, element, attrs) {
+				var property = attrs.ldInputFocus;
+				var isFocus = $parse(property);
+				scope.$watch(isFocus, function (newValue) {
+					console.log(!!newValue);
+					if (!!newValue) {
+						$timeout(function() {
+							element[0].focus();
+						});
+					}
+				});
+			}
+		};
+	}]);
+
+'use strict';
+
+/**
+ * @ngdoc directive
  * @name directive:ldMenu
  * @description
  * # sidebar menu directive
