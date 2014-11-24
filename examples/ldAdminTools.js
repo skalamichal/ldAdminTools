48c9e7bbae3ae20701a63841a74ef400b8785eca
Michal Skala <michal@mskala.com>
2014-11-24 17:49:54 +0100

    the ld-slide-left directive removed, can be replaced by css and $animate service.

--- examples/ldAdminTools.js
+++ examples/ldAdminTools.js
@@ -1,6 +1,6 @@
 ; (function(angular){
 'use strict';
-angular.module('ldAdminTools', ['ui.bootstrap', 'RecursionHelper', 'LocalStorageModule']); 
+angular.module('ldAdminTools', ['ngAnimate', 'ui.bootstrap', 'RecursionHelper', 'LocalStorageModule']); 
 'use strict';
 
 /**
@@ -675,174 +675,187 @@ angular.module('ldAdminTools')
 		// the element is by default not closeable
 		isCloseable: false
 	})
-	.controller('ldSlideLeftController', ['$scope', '$transition', '$attrs', '$parse', 'ldSlideLeftConfig', function ($scope, $transition, $attrs, $parse, config) {
-		var self = this;
-		var scope = $scope.$new();
-		var currentTransition;
+	.controller('ldSlideLeftController', ['$scope', '$transition', '$attrs', '$parse', '$log', 'ldSlideLeftConfig',
+		function ($scope, $transition, $attrs, $parse, $log, config) {
+			var self = this;
+			//var scope = $scope.$new();
+			var currentTransition;
 
-		// getter and setter for the ldAnimated value
-		var getIsAnimated;
-		var setIsAnimated = angular.noop;
+			// getter and setter for the ldAnimated value
+			var getIsAnimated;
+			var setIsAnimated = angular.noop;
 
-		// getter and setter for the ldSlideLeft value, which has the open/close state
-		var getIsOpen = $parse($attrs.ldSlideLeft);
-		var setIsOpen = getIsOpen.assign;
+			// getter and setter for the ldSlideLeft value, which has the open/close state
+			var getIsOpen = $parse($attrs.ldSlideLeft);
+			var setIsOpen = getIsOpen.assign;
 
-		// getter and setter for the ldCloseable value
-		var getIsCloseable;
-		var setIsCloseable = angular.noop;
+			// getter and setter for the ldCloseable value
+			var getIsCloseable;
+			var setIsCloseable = angular.noop;
 
-		// default properties
-		var width = 0;
-		var initialAnimSkip = true;
+			// default properties
+			var width = 0;
+			var initialAnimSkip = true;
 
-		// these properties may be defined in the directive, check if they are defined and get the value from them
-		// or use the config value
-		scope.isAnimated = angular.isDefined($attrs.ldAnimated) ? $scope.$eval($attrs.ldAnimated) : config.isAnimated;
-		scope.isCloseable = angular.isDefined($attrs.ldCloseable) ? $scope.$eval($attrs.ldCloseable) : config.isCloseable;
-		scope.isOpen = $scope.$eval($attrs.ldSlideLeft);
+			// these properties may be defined in the directive, check if they are defined and get the value from them
+			// or use the config value
+			var isAnimated = angular.isDefined($attrs.ldAnimated) ? $scope.$eval($attrs.ldAnimated) : config.isAnimated;
+			var isCloseable = angular.isDefined($attrs.ldCloseable) ? $scope.$eval($attrs.ldCloseable) : config.isCloseable;
+			var isOpen = $scope.$eval($attrs.ldSlideLeft);
 
-		// initialize the controller with element for sliding
-		this.init = function (element) {
-			self.$element = element;
-			width = self.$element[0].offsetWidth;
+			// initialize the controller with element for sliding
+			this.init = function (element) {
+				self.$element = element;
+				width = self.$element[0].offsetWidth;
 
-			// check if ld-isAnimated is set and set watcher function to handle changes
-			if (angular.isDefined($attrs.ldAnimated)) {
-				getIsAnimated = $parse($attrs.ldAnimated);
-				setIsAnimated = getIsAnimated.assign;
+				// check if ld-isAnimated is set and set watcher function to handle changes
+				if (angular.isDefined($attrs.ldAnimated)) {
+					getIsAnimated = $parse($attrs.ldAnimated);
+					setIsAnimated = getIsAnimated.assign;
 
-				// watch the attribute in the parent scope
-				$scope.$watch(getIsAnimated, function (value) {
+					// watch the attribute in the parent scope
+					$scope.$watch(getIsAnimated, function (value) {
+						// and update our scope variable so we can watch it's state
+						isAnimated = !!value;
+					});
+				}
+
+				// check if ld-isCloseable is set and set watcher function to handle changes
+				if (angular.isDefined($attrs.ldCloseable)) {
+					getIsCloseable = $parse($attrs.ldCloseable);
+					setIsCloseable = getIsCloseable.assign;
+
+					// watch the attribute in the parent scope
+					$scope.$watch(getIsCloseable, function (value) {
+						// and update our scope variable so we can watch it's state
+						isCloseable = !!value;
+					});
+				}
+
+				// the ld-slide-left is always set, because it's the directive name
+				// watch the parent scope value
+				$scope.$watch($attrs.ldSlideLeft, function (open) {
 					// and update our scope variable so we can watch it's state
-					scope.isAnimated = !!value;
+					isOpen = !!open;
 				});
+			};
+
+			// public controller API method
+			this.toggle = function (open) {
+				isOpen = arguments.length ? !!open : !isOpen;
+				// update the parent scope as well
+				setIsOpen($scope, isOpen);
+			};
+
+			// perform the transition
+			function doTransition(change) {
+				function newTransitionDone() {
+					// Make sure it's this transition, otherwise, leave it alone.
+					if (currentTransition === newTransition) {
+						currentTransition = undefined;
+					}
+				}
+
+				var newTransition = $transition(self.$element, change);
+				if (currentTransition) {
+					currentTransition.cancel();
+				}
+				currentTransition = newTransition;
+				newTransition.then(newTransitionDone, newTransitionDone);
+				return newTransition;
+
 			}
 
-			// check if ld-isCloseable is set and set watcher function to handle changes
-			if (angular.isDefined($attrs.ldCloseable)) {
-				getIsCloseable = $parse($attrs.ldCloseable);
-				setIsCloseable = getIsCloseable.assign;
+			// expand (show) the element
+			this.expand = function () {
+				if (!isAnimated || initialAnimSkip) {
+					initialAnimSkip = false;
+					expandDone();
+				}
+				else {
+					self.$element.removeClass('ld-slide');
+					self.$element.addClass('ld-sliding-left');
+					$log.debug('Expand - Left:');
+					var a = doTransition({left: 0 + 'px'}).then(expandDone);
+					$log.debug(a);
+				}
+			};
 
-				// watch the attribute in the parent scope
-				$scope.$watch(getIsCloseable, function(value) {
-					// and update our scope variable so we can watch it's state
-					scope.isCloseable = !!value;
-				});
+			// transition is done
+			function expandDone() {
+				$log.debug('Expand done');
+				self.$element.css('left', '0px');
+				self.$element.removeClass('ld-sliding-left');
+				self.$element.addClass('ld-slide');
+				self.$element.addClass('in');
 			}
 
-			// the ld-slide-left is always set, because it's the directive name
-			// watch the parent scope value
-			$scope.$watch($attrs.ldSlideLeft, function (open) {
-				// and update our scope variable so we can watch it's state
-				scope.isOpen = !!open;
-			});
-		};
+			// slide (close) the element
+			this.slide = function () {
+				if (initialAnimSkip || !isAnimated) {
+					initialAnimSkip = false;
+					slideDone();
+				}
+				else {
+					// CSS transitions don't work with width: auto, so we have to manually change the height to a specific value
+					self.$element.css({left: 0 + 'px'});
+					//trigger reflow so a browser realizes that height was updated from auto to a specific value
+					/*jshint unused:false*/
+					var x = self.$element[0].offsetLeft;
 
-		// public controller API method
-		this.toggle = function (open) {
-			scope.isOpen = arguments.length ? !!open : !scope.isOpen;
-			// update the parent scope as well
-			setIsOpen($scope, scope.isOpen);
-		};
+					$log.debug('Slide - Left: ' + (-width));
+					self.$element.removeClass('ld-slide');
+					self.$element.removeClass('in');
+					self.$element.addClass('ld-sliding-left');
 
-		// perform the transition
-		function doTransition(change) {
-			function newTransitionDone() {
-				// Make sure it's this transition, otherwise, leave it alone.
-				if (currentTransition === newTransition) {
-					currentTransition = undefined;
+					doTransition({left: -width + 'px'}).then(slideDone);
+				}
+			};
+
+			// transition is done
+			function slideDone() {
+				$log.debug('Slide done');
+				self.$element.css({left: -width + 'px'});
+				self.$element.removeClass('ld-sliding-left');
+				self.$element.removeClass('in');
+				self.$element.addClass('ld-slide');
+			}
+
+			// update the element based on the isOpen state
+			function update() {
+				if (isOpen) {
+					self.expand();
+				}
+				else {
+					self.slide();
 				}
 			}
 
-			var newTransition = $transition(self.$element, change);
-			if (currentTransition) {
-				currentTransition.cancel();
-			}
-			currentTransition = newTransition;
-			newTransition.then(newTransitionDone, newTransitionDone);
-			return newTransition;
+			// watch for the isOpen scope variable changes
+			$scope.$watch(function () {
+					return isOpen;
+				},
+				function (shouldOpen) {
+					// if not isCloseable and we should close, it's not allowed, switch back to isOpen true
+					if (!isCloseable && !shouldOpen) {
+						isOpen = true;
+						setIsOpen($scope, isOpen);
+					}
+					update();
+				}
+			);
 
-		}
-
-		// expand (show) the element
-		this.expand = function () {
-			if (!scope.isAnimated || initialAnimSkip) {
-				initialAnimSkip = false;
-				expandDone();
-			}
-			else {
-				self.$element.removeClass('ld-slide').addClass('ld-sliding-left');
-				doTransition({ left: 0 + 'px' }).then(expandDone);
-			}
-		};
-
-		// transition is done
-		function expandDone() {
-			self.$element.css('left', '0px');
-			self.$element.removeClass('ld-sliding-left');
-			self.$element.addClass('ld-slide').addClass('in');
-		}
-
-		// slide (close) the element
-		this.slide = function () {
-			if (initialAnimSkip || !scope.isAnimated) {
-				initialAnimSkip = false;
-				slideDone();
-				self.$element.css({left: -width + 'px'});
-			}
-			else {
-				// CSS transitions don't work with width: auto, so we have to manually change the height to a specific value
-				self.$element.css({ left: 0 + 'px' });
-				//trigger reflow so a browser realizes that height was updated from auto to a specific value
-				/*jshint unused:false*/
-				var x = self.$element[0].offsetLeft;
-
-				self.$element.removeClass('ld-slide').removeClass('in').addClass('ld-sliding-left');
-
-				doTransition({ left: -width + 'px' }).then(slideDone);
-			}
-		};
-
-		// transition is done
-		function slideDone() {
-			self.$element.css('left', '-' + width + 'px');
-			self.$element.removeClass('ld-sliding-left').removeClass('in');
-			self.$element.addClass('ld-slide');
-		}
-
-		// update the element based on the isOpen state
-		function update() {
-			if (scope.isOpen) {
-				self.expand();
-			}
-			else {
-				self.slide();
-			}
-		}
-
-		// watch for the isOpen scope variable changes
-		scope.$watch('isOpen', function(shouldOpen) {
-			// if not isCloseable and we should close, it's not allowed, switch back to isOpen true
-			if (!scope.isCloseable && !shouldOpen) {
-				scope.isOpen = true;
-				setIsOpen($scope, scope.isOpen);
-			}
-			update();
-		});
-
-		// watch for the isCloseable scope variable changes
-		scope.$watch('isCloseable', function(closeable) {
-			if (!closeable && !scope.isOpen)
-			{
-				scope.isOpen = true;
-				setIsOpen($scope, scope.isOpen);
-			}
-			update();
-
-		});
-
-	}])
+			// watch for the isCloseable scope variable changes
+			$scope.$watch(function () {
+				return isCloseable;
+			}, function (closeable) {
+				if (!closeable && !isOpen) {
+					isOpen = true;
+					setIsOpen($scope, isOpen);
+				}
+				update();
+			});
+		}])
 	.directive('ldSlideLeft', [function () {
 		return {
 			restrict: 'A',
@@ -854,8 +867,7 @@ angular.module('ldAdminTools')
 			}
 		};
 	}])
-	.directive('ldSlideToggle', [function()
-	{
+	.directive('ldSlideToggle', [function () {
 		return {
 			require: '?^ldSlideLeft',
 			/*jshint unused:false*/
@@ -866,7 +878,7 @@ angular.module('ldAdminTools')
 
 				// toggle the element visibility (slide it)
 				function toggleSlide() {
-					scope.$apply(function() {
+					scope.$apply(function () {
 						slideController.toggle();
 					});
 				}
