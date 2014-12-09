194cc9970893bf4ca06241d71368b8f41c9ddd1c
Michal Skala <michal@mskala.com>
2014-12-09 21:00:46 +0100

    ldResize test added

--- test/spec/directives/ldresize.js
+++ test/spec/directives/ldresize.js
@@ -3,13 +3,46 @@
 describe('Directive: ldResize', function () {
 
 	// load the directive's module
-	beforeEach(module('ldAdminTools'));
+	var windowMock;
+	beforeEach(module('ldAdminTools', function ($provide) {
+		windowMock = {
+			innerWidth: 800,
+			innerHeight: 600,
+			setSize: function(width, height) {
+				windowMock.innerWidth = width;
+				windowMock.innerHeight = height;
+			},
+			bind: jasmine.createSpy('bind'),
+			addEventListener: jasmine.createSpy('addEventListener')
+		};
 
-	var element,
-		scope;
-
-	beforeEach(inject(function ($rootScope) {
-		scope = $rootScope.$new();
+		$provide.value('$window', windowMock);
 	}));
+
+	var $scope,
+		$window,
+		$compile,
+		$rootScope;
+
+	beforeEach(inject(function (_$rootScope_, _$window_, _$compile_) {
+		$rootScope = _$rootScope_;
+		$scope = _$rootScope_.$new();
+		$window = _$window_;
+		$compile = _$compile_;
+	}));
+
+	it('should call resize method, when window is resized', function () {
+		$rootScope.onResize = jasmine.createSpy('onResize');
+		$compile('<div ld-resize="onResize"></div>')($scope);
+		$rootScope.$digest();
+
+		$window.setSize(300,200);
+		$rootScope.$digest();
+
+		$scope.updateSize();
+		$rootScope.$digest();
+
+		expect($rootScope.onResize).toHaveBeenCalledWith(300, 200);
+	});
 
 });
